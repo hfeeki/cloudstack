fc03fad892bccfac217877c93a8fc4e5e0920dcd
Wagner Souza <wagnersza@gmail.com>
2012-07-19 18:43:05 -0700

    add vagrant files

---
+++ vagrant/manifests/nodes.pp
@@ -0,0 +1,15 @@
+node basenode {
+  exec { "route add -net 10.2.45.0 netmask 255.255.255.0 gw 10.10.10.1 eth1":
+          unless => 'netstat -nrv | grep -e "10.2.45.0.*10.10.10.1"',
+  }
+}
+
+node cloudstackms.example.com inherits basenode {
+  include cloudstack::managementserver
+  # Install all management server resources in a single node
+}
+
+node cloudstackhv.example.com inherits basenode {
+  include cloudstack::hypervisor::kvm
+  # Install KVM hypervisor
+}
