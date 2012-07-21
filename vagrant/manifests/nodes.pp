9c69ddc812f208f038d70e0a44d75611f76e1478
Wagner Souza <wagnersza@gmail.com>
2012-07-21 13:56:04 -0700

    change nodes conf

--- vagrant/manifests/nodes.pp
+++ vagrant/manifests/nodes.pp
@@ -1,15 +1,17 @@
 node basenode {
-  exec { "route add -net 10.2.45.0 netmask 255.255.255.0 gw 10.10.10.1 eth1":
-          unless => 'netstat -nrv | grep -e "10.2.45.0.*10.10.10.1"',
-  }
+  # exec { "route add -net 10.2.45.0 netmask 255.255.255.0 gw 10.10.10.1 eth1":
+  #         unless => 'netstat -nrv | grep -e "10.2.45.0.*10.10.10.1"',
+  # }
 }
 
+# Install all management server resources in a single node
 node cloudstackms.example.com inherits basenode {
   include cloudstack::management
-  # Install all management server resources in a single node
+  include cloudstack::db
+  include cloudstack::nfs
 }
 
+# Install KVM hypervisor
 node cloudstackhv.example.com inherits basenode {
   include cloudstack::hypervisor
-  # Install KVM hypervisor
 }
